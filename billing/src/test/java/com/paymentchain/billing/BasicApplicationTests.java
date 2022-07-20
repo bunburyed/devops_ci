ebee8ab45c7e1a00a3c9f01d6bbec695885a9f33
Edgar Estrada Franco <bunburyed1992@gmail.com>
2022-07-19 22:53:15 -0500

    add test modification

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,7 +1,9 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.postgresql.traslation.messages_bg;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,7 +13,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String message= "default message cambio test devops";
+		String meessage= "default message cambio test devops";
 		Assert.assertNotNull(meessage);
 	}
 
